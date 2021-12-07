7c0e26583cc6060092a4e07a446d5168fa85f003
Kould <2815680434@qq.com>
2021-12-07 16:44:50 +0800

    argsEncode编码优化

--- src/main/java/com/kould/encoder/impl/BaseCacheEncoder.java
+++ src/main/java/com/kould/encoder/impl/BaseCacheEncoder.java
@@ -13,6 +13,8 @@ import java.util.Map;
 
 public class BaseCacheEncoder extends CacheEncoder {
 
+    private static final char[] BYTE_CHARS = new char[]{'0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'a', 'b', 'c', 'd', 'e', 'f'};
+
     private static final BaseCacheEncoder INSTANCE = new BaseCacheEncoder() ;
 
     private BaseCacheEncoder() {}
@@ -39,7 +41,7 @@ public class BaseCacheEncoder extends CacheEncoder {
         //需要寻找循环依赖序列化方案-》Mybatis-Plus的Wrapper
         //new:使用Kryo序列化
         byte[] shas = getDigest("SHA").digest(KryoUtil.writeToByteArray(args));
-        return new String(shas) ;
+        return encodeByte(shas).toString();
     }
 
     @Override
@@ -71,9 +73,15 @@ public class BaseCacheEncoder extends CacheEncoder {
         return INSTANCE;
     }
 
-    /*
-    取自org.springframework.data.redis.core.script.DigestUtils
-     */
+    private static String encodeByte(byte[] data) {
+        StringBuilder stringBuilder = new StringBuilder(20) ;
+        for (byte datum : data) {
+            //二进制编码转换
+            stringBuilder.append(BYTE_CHARS[15 & datum]);
+        }
+        return stringBuilder.toString();
+    }
+
     private static MessageDigest getDigest(String algorithm) {
         try {
             return MessageDigest.getInstance(algorithm);
