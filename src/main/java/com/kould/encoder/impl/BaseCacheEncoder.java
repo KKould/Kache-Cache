29a1f2b4432bdc9d9809b39a23e392faa2851129
Kould <2815680434@qq.com>
2021-12-13 17:21:25 +0800

    1、BaseCacheEncoder使用Hash优化缩短参数长度

--- src/main/java/com/kould/encoder/impl/BaseCacheEncoder.java
+++ src/main/java/com/kould/encoder/impl/BaseCacheEncoder.java
@@ -14,6 +14,10 @@ public class BaseCacheEncoder extends CacheEncoder {
         return INSTANCE ;
     }
 
+    private static final int P = 16777619;
+
+    private static final int HASH = (int)2166136261L ;
+
 
     @Override
     public String encode(String serviceMethodStatus, String serviceMethod, String daoEnityName, String daoMethodName, String daoArgs) {
@@ -31,7 +35,7 @@ public class BaseCacheEncoder extends CacheEncoder {
         //此处可以返回空但是空会导致同一Service方法内调用同一Dao方法且Dao方法的参数不一致时会导致缓存误差
         //需要寻找循环依赖序列化方案-》Mybatis-Plus的Wrapper
         //new:使用Kryo序列化
-        return KryoUtil.writeToString(args);
+        return String.valueOf(getHash(KryoUtil.writeToByteArray(args)));
     }
 
     @Override
@@ -41,5 +45,22 @@ public class BaseCacheEncoder extends CacheEncoder {
 
     private Object readResolve() {
         return INSTANCE;
+    }
+
+    private static int getHash(byte[] bytes){
+        int hash = HASH;
+        for (byte datum : bytes) {
+            hash = (hash ^ datum * P);
+            hash += hash << 13;
+            hash ^= hash >> 7;
+            hash += hash << 3;
+            hash ^= hash >> 17;
+            hash += hash << 5;
+        }
+        // 如果算出来的值为负数则取其绝对值
+        if (hash < 0) {
+            hash = Math.abs(hash);
+        }
+        return hash;
     }
 }
