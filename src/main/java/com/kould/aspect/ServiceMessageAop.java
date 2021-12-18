c1715d066bcbc3adcfe3ae6ea7fa510206a8791b
Kould <2815680434@qq.com>
2021-12-18 13:32:04 +0800

    修复Aop定位问题

--- src/main/java/com/kould/aspect/ServiceMessageAop.java
+++ src/main/java/com/kould/aspect/ServiceMessageAop.java
@@ -1,14 +1,11 @@
 package com.kould.aspect;
 
 import com.kould.annotation.CacheBeanClass;
-import com.kould.annotation.CacheChange;
-import com.kould.annotation.ServiceCache;
 import com.kould.config.KacheAutoConfig;
 import com.kould.message.KacheMessage;
 import org.aspectj.lang.ProceedingJoinPoint;
 import org.aspectj.lang.annotation.Around;
 import org.aspectj.lang.annotation.Aspect;
-import org.aspectj.lang.annotation.Pointcut;
 import org.aspectj.lang.reflect.MethodSignature;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.annotation.Order;
@@ -26,10 +23,6 @@ public final class ServiceMessageAop {
     @Autowired
     private KacheAutoConfig kacheAutoConfig;
 
-    @Pointcut(KacheAutoConfig.POINTCUT_EXPRESSION_SERVICE)
-    public void pointCut() {
-    }
-
     /**
      * 为DAO层缓存AOP提供以下数据
      *  1、该织入点方法
@@ -41,26 +34,24 @@ public final class ServiceMessageAop {
      * @return point执行，不影响其正常工作
      * @throws Throwable
      */
-    @Around("@annotation(com.kould.annotation.CacheBeanClass) || pointCut()")
+    @Around("@annotation(com.kould.annotation.ServiceCache) || @annotation(com.kould.annotation.CacheChange)")
     public Object findArroundInvoke(ProceedingJoinPoint point) throws Throwable {
         Class<?> targetClass = point.getTarget().getClass() ;
         Method method = null ;
         MethodSignature methodSignature = (MethodSignature) point.getSignature();
         method = methodSignature.getMethod() ;
-        //注解检测
-        if (method.isAnnotationPresent(ServiceCache.class) || method.isAnnotationPresent(CacheChange.class)) {
-            CacheBeanClass cacheBeanClass = targetClass.getAnnotation(CacheBeanClass.class);
-            Class<?> cacheClass = cacheBeanClass.clazz();
-            //传递Service方法签名
-            DaoCacheAop.MESSAGE_THREAD_LOCAL_VAR.set(KacheMessage.builder()
-                    .method(method)
-                    .clazz(targetClass)
-                    .cacheClazz(cacheClass)
-                    .methodName(method.getName())
-                    .build());
-            return point.proceed();
-        } else {
-            return point.proceed() ;
+        CacheBeanClass cacheBeanClass = targetClass.getAnnotation(CacheBeanClass.class);
+        if (cacheBeanClass == null) {
+            throw new Exception("未标注CacheBeanClass") ;
         }
+        Class<?> cacheClass = cacheBeanClass.clazz();
+        //传递Service方法签名
+        DaoCacheAop.MESSAGE_THREAD_LOCAL_VAR.set(KacheMessage.builder()
+                .method(method)
+                .clazz(targetClass)
+                .cacheClazz(cacheClass)
+                .methodName(method.getName())
+                .build());
+        return point.proceed();
     }
 }
